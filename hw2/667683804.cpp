00963e372114221cb18d14874b54efd4e64351bd
basheersubei <basheersubei@gmail.com>
2014-10-11 17:18:38 -0500

    moved frequency_of_primes definition to misc.

--- hw2/667683804.cpp
+++ hw2/667683804.cpp
@@ -246,14 +246,13 @@ int randomIntWithMax(int max)
   return rand() % max;
 }
 
-/* * * * * * * * * * END misc. function definitions * * * * * * * * */
-
 int frequency_of_primes (int n) {
   int i,j;
   int freq=n-1;
   for (i=2; i<=n; ++i) for (j=sqrt(i);j>1;--j) if (i%j==0) {--freq; break;}
   return freq;
 }
+/* * * * * * * * * * END misc. function definitions * * * * * * * * */
 
 int main()
 {
@@ -301,4 +300,4 @@ int main()
 	}
 
 
-}
+}// end main()
