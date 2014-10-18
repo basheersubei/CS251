b64255d1d9e46e74f7cc9748e6f1781b0645e1d9
basheersubei <basheersubei@gmail.com>
2014-10-18 18:29:14 -0500

    hacked a bunch of string width thingies based on level

--- hw3/bsubei2.cpp
+++ hw3/bsubei2.cpp
@@ -14,6 +14,8 @@
           * Note that only the displayQueue method is rewritten for this assignment.
 
           * This program displays a binary tree (including edges) in ASCII.
+          * A max screen size of 80 columns wide and 50 rows high is assumed.
+          * Elements in the tree are assumed to be limited to 4-digit width.
 
           * -------------------------------------------------
 
@@ -37,6 +39,7 @@
 
 #include <iostream>
 #include <cmath>
+#include <iomanip>  
 using namespace std;
 
 // define global constants and structures
@@ -45,6 +48,98 @@ using namespace std;
 void displayQueue( int q[], int size)
 {
     //TODO actual assignment #3 code goes here
+
+    /* proposed method:
+     * 1) find the widest row (the level with the most siblings)
+     * 2) center all the tree based on that widest level (the positions of elements
+     * in this level would be multiples of max_width / number_of_elements_in_level). Possibly
+     * consider just using the lowest level for centering.
+     * 3) the parents of each element pair is centered between its children...
+
+
+
+    */
+
+    // int level = log2(size); // get bottom level in the heap
+    // int siblings = (2 << (level-1)); // get how many siblings bottom level should have (assuming full)
+
+    for( int i=1; i<size; i++) {
+
+        int level = log2(i); // get level in the heap
+        int siblings = (2 << (level-1)); // get how many siblings this level should have (assuming full)
+        int padding_interval = (siblings > 0) ? (80-siblings*4) / (siblings+1): 42;
+        padding_interval = (padding_interval<0) ? 0 : padding_interval;
+
+        int next_level = level + 1;
+        int next_siblings = (2 << (next_level-1));
+        int next_padding_interval = (next_siblings > 1) ? (80- next_siblings*4) / (next_siblings+1) : 40;
+        next_padding_interval += 4;
+        next_padding_interval = (next_padding_interval<0) ? 0 : next_padding_interval;
+
+        // here, we use padding_interval - next_padding_interval to represent how many underscores to print
+
+        // Display newline between levels if current value is power of 2
+        //    logb gives floating point log, and log2 is integer log value.
+        if( logb(i) == log2(i)) {
+            // cout << i << endl;
+            cout << endl;
+            if(i != 1){
+
+                int pad_right = next_padding_interval*2+2;
+                int pad_left = padding_interval + 4;
+                if(padding_interval<4)
+                    pad_left = 5;
+                if(next_padding_interval<4)
+                    pad_right = 5;
+
+                for(int j=i; j<i+siblings ; j++){
+                    
+                    if(j % 2 == 0){
+                       
+                        if(j>15)
+                            cout << setw(pad_left) << "|";
+                        else
+                            cout << setw(pad_left) << "/";
+
+                    }else{
+                        if(j>15)
+                            cout << setw(pad_right) << "|";
+                        else
+                            cout << setw(pad_right) << "\\";
+  
+                    }
+                }
+
+            }
+
+
+            cout << endl;
+
+            // cout << setw(40);
+
+        }
+        // cout << setfill('_');
+        // for(int j=0;j<padding_interval;j++)
+        //     cout << "_";
+
+        // print padding on the left and give it a certain width
+        // the string constructor here just creates a string of underscores with length padding_interval - next_padding_interval
+        
+        if(padding_interval-next_padding_interval <1){
+            cout << setw(4) << q[i] << " ";
+        }
+        else{
+            cout << setw(padding_interval) << std::string(padding_interval-next_padding_interval, '_');
+            cout << setw(4) << q[i]; // print the element and assume width of 4
+            // print padding on the right
+            cout << std::string(padding_interval-next_padding_interval, '_');
+
+        }
+
+
+
+    }
+    cout << endl;   // make sure output buffer is flushed
 }
 
 
@@ -127,7 +222,8 @@ void qInsert( int q[],     // The queue, implemented as an array
     // Sanity check for maximium size
     if( qSize >= MAX_SIZE) {
         cout << "***Error: queue size exceeded.  Exiting program." << endl;
-        exit( -1);
+        return;
+        // exit( -1);
     }
     else {
         q[ qSize] = x;        // add element to end of queue
@@ -197,7 +293,8 @@ int getMin(int q[],      // The queue, implemented as an array
     if( qSize <= 1) {
         cout << "*** Error: Can't remove from empty queue.  Exiting program."
              << endl;
-        exit( -1);
+        return -1;
+        // exit( -1);
     }
     else {
         returnValue = q[1];  // Set return value to be min value, which is root
@@ -232,22 +329,24 @@ int main() {
     
     cout << "Starting Heap program \n";
     
-    int numberOfValues = 10;
-    int valuesToAdd[] = {1783,1776,1492,1804,1865,1945,1963,1918,2001,1941};
+    int numberOfValues = 31;
+    int valuesToAdd[] = {1783,1776,1492,1804,1865,1945,1963,1918,2001,1941,9000,9001,9002,9003,9004,9005,9006,9007,9008,9009,9010,9011,9012,9013,9014,9015,9016,9017,9018,9019,9020};
 
     for( int i=0; i<numberOfValues; i++) {
         qInsert( q, qSize, valuesToAdd[ i]);
     }
     
     // Display the values in the queue, by level
-    displayQueueUgly(q, qSize);
+    // displayQueueUgly(q, qSize);
+    displayQueue(q, qSize);
     
     // Successively get the minimum value until tree is empty
-    cout << "\nSuccessively displaying and removing min values" << endl;
-    while( qSize > 1) {
-        cout << "Removing:" << getMin( q, qSize) << "\n";
-        displayQueueUgly( q, qSize);
-    }
+    // cout << "\nSuccessively displaying and removing min values" << endl;
+    // while( qSize > 1) {
+    //     cout << "Removing:" << getMin( q, qSize) << "\n";
+    //     // displayQueueUgly( q, qSize);
+    //     displayQueue( q, qSize);
+    // }
     
     cout << "\n\nDone with program...\n";
     return 0;
