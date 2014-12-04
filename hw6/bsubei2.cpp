071cfced34211023b530069a92fa01383e26f9bd
Basheer Subei <basheersubei@gmail.com>
2014-12-03 22:55:56 -0600

    refactored all code into one source file for submission only.

--- hw6/bsubei2.cpp
+++ hw6/bsubei2.cpp
@@ -16,7 +16,71 @@
 
 */
 
-#include "./bsubei2.h"
+
+#include <stdio.h>  // for compiling on Unix
+#include <stdlib.h>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>    // For file input
+#include <cassert>    // for assertions
+#include <cstring>
+
+// preprocessor definitions
+
+// debug mode (prints out debug messages)
+#define DEBUG_MODE 0
+#define TEST_DATA_FILES 0
+#define MAX_LINE_LENGTH 10000
+#define ASCII_OFFSET 97
+#define NUMBER_OF_CHILDREN 26
+
+// trie nodes have child pointers and sibling pointers,
+// as well as parent pointer (at the end of the linked list).
+// defines each node in the trie
+struct Node {
+    char c;  // the char this node holds
+    bool is_word;  // indicates whether it's a complete word
+    Node* pChild;  // child pointer (points to another node under it)
+    Node* pSibling;  // sibling pointer (points to node to the right of it)
+};
+
+// defines a node to store word strings in a linked list
+struct WordNode {
+    char word[MAX_LINE_LENGTH];
+    WordNode *pNext;
+};
+
+// function declarations
+void printStartSequence();
+void printOptions();
+void readDictionary(Node* word_trie);
+char * strReverse(char *str);
+void storeWordInTrie(char *word, int size, Node* trie);
+int askForSuffix(char* suffix);
+void searchTrieForSuffix(Node* word_trie,
+    char* suffix,
+    int length,
+    WordNode* words_found);
+void searchWordInTrie(char *word,
+    int size,
+    Node* trie,
+    int &how_many_found,
+    char *word_so_far,
+    WordNode *words_found);
+void deleteTrieWords(Node* word_trie);
+void TriePreorderTraversal(char *word_so_far,
+    int &how_many_found,
+    WordNode *words_found,
+    Node* trie);
+void addWordNode(WordNode *current_node, char* word_to_add);
+void printLinkedList(WordNode *current_node);
+void deleteWordNodes(WordNode *current_node);
+void findStr(char *suffix_string, Node* &pSuffix, Node *trie);
+bool findNextWord(Node *pSuffix, Node* &pCursor);
+bool shiftCursorOnce(Node *pSuffix, Node* &pCursor);
+void printWord(Node* pCursor);
+void printDetailedOptions();
+bool deleteWordFromTrie(char *word, int size, Node *trie);
 
 // avoiding using namespace, instead explicitly stating using using
 using std::cin;
@@ -24,7 +88,7 @@ using std::cout;
 using std::endl;
 using std::ifstream;
 
-void runProgram() {
+int main() {
     // print welcome message and stuff
     printStartSequence();
 
@@ -191,7 +255,9 @@ void runProgram() {
 
     // delete all nodes in trie
     deleteTrieWords(word_trie);
-}  // end runProgram()
+
+    return 0;
+}  // end main()
 
 // deletes word from Trie
 // the main idea (pseudocode) is:
